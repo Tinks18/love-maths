9c8233c4d5c45e534b603fb6d1fe4e6b6eb4247a
Tinks18 <tinutani9@gmail.com>
2022-06-29 02:41:48 +0000

    Add code for subtract

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,8 @@ function runGame(gameType){
     displayAdditionQuestion(num1, num2);
    }else if (gameType === "multiply") {
     displayMultiplyQuestion(num1, num2);
+   }else if (gameType === "subtract") {
+    displaySubtractQuestion(num1, num2);
    }
    else{
     alert(`unknown game type: ${gameType}`);
@@ -70,6 +72,10 @@ function calculateCorrectAnswer(){
     } else if ( operator === "x") {
 
         return [operand1 * operand2, "multiply"];
+
+    } else if (operator === "-")  {
+            return [operand1 - operand2, "subtract"];
+               
     }
         
     else {
@@ -101,8 +107,11 @@ function displayAdditionQuestion(operand1, operand2){
 
 }
 
-function displaySubtractQuestion(){
-    
+function displaySubtractQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1:operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2:operand1;
+    document.getElementById('operator').textContent = "-";
+
 }
 
 function displayMultiplyQuestion(operand1, operand2){
